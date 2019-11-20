f37610a953eede5df78395581e6f57df23669de5
Manuel Resinas <resinas@us.es>
2019-11-20 14:24:44 +0000

    Removed _id from the results of GET /contacts

--- index.js
+++ index.js
@@ -28,7 +28,10 @@ app.get(BASE_API_PATH + "/contacts", (req, res) => {
             console.log(Date() + "-" + err);
             res.sendStatus(500);
         } else {
-            res.send(contacts);
+            res.send(contacts.map((contact) => {
+                delete contact._id;
+                return contact;
+            }));
         }
     });
 
