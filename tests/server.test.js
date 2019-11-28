6ced01b8688bcc876db19ff5a1d888396749e5f2
Manuel Resinas <resinas@us.es>
2019-11-28 16:17:04 +0000

    Step 1 - Test structure created

---
+++ tests/server.test.js
@@ -0,0 +1,12 @@
+
+describe("Hello world tests", () => {
+
+    it("Should do an stupid test", () => {
+        const a = 5;
+        const b = 3;
+        const sum = a - b;
+
+        expect(sum).toBe(8);
+    });
+
+});
