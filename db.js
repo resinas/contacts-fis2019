c77a73df4be6a52dd80e529f021d39162654b907
Manuel Resinas <resinas@us.es>
2021-11-10 17:27:41 +0100

    Fix connection URL

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const ENV_URL = 'mongodb://' + process.env.MONGODB_USERNAME+':'+process.env.MONGODB_PASSWORD+"//"+process.env.MONGODB_HOST+"/"+process.env.MONGODB_DATABASE;
+const ENV_URL = 'mongodb://' + process.env.MONGODB_USERNAME+':'+process.env.MONGODB_PASSWORD+"@"+process.env.MONGODB_HOST+"/"+process.env.MONGODB_DATABASE;
 console.log("ENV:"+ENV_URL);
 const DB_URL = (! process.env.MONGODB_HOST) ? 'mongodb://mongo/test' : ENV_URL;
 
